eb89c85c237ecba1afaf30b6d5e59b2852d7589f
Gregor Woiwode <gw@co-it.eu>
2017-06-20 13:17:06 +0200

    (directive:click-once) use click-once in book module

---
+++ neuhausen-2017-06-19/src/app/utilities/event-modifiers/click-once.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, ElementRef, Renderer2, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Directive({
+  // tslint:disable-next-line:directive-selector
+  selector: '[click-once]'
+})
+export class ClickOnceDirective implements OnInit {
+  // (click-once) || (clickOnce)
+  // tslint:disable-next-line:no-output-rename
+  @Output('click-once') clickOnce = new EventEmitter<Event>();
+
+  unsubscribe: Function;
+
+  constructor(
+    private renderer: Renderer2,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit() {
+    this.unsubscribe = this.renderer.listen(
+      this.element.nativeElement,
+      'click',
+      event => {
+        const parent = this.element.nativeElement.parentNode
+        parent.childNodes.map((elem) => {
+          this.renderer.setAttribute(elem, 'disabled', 'true');
+        })
+        this.clickOnce.emit(event);
+        this.unsubscribe();
+      }
+    );
+  }
+}
---
+++ neuhausen-2017-06-19/src/app/utilities/utilities.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClickOnceDirective } from './event-modifiers/click-once.directive';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ClickOnceDirective],
+  exports: [ClickOnceDirective]
+})
+export class UtilitiesModule { }
