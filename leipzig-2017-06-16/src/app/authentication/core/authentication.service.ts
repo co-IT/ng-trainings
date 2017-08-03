02243e56db0fff1cb9ce693ff771751567d7a6c9
Gregor Woiwode <gw@co-it.eu>
2017-08-03 07:09:35 +0200

    (fix) downgrade typescript

--- leipzig-2017-06-16/src/app/authentication/core/authentication.service.ts
+++ leipzig-2017-06-16/src/app/authentication/core/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { JwtHelper} from 'angular2-jwt' ;
+import { JwtHelper } from 'angular2-jwt';
+
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthenticationService {
@@ -10,16 +12,16 @@ export class AuthenticationService {
   constructor(private http: Http) { }
 
   login(username: string, password: string) {
-    return this.http.post('http://localhost:4280/auth/login', {user: username, password: password})
-    .map(res => res.json());
+    return this.http
+      .post('http://localhost:4280/auth/login', { user: username, password })
+      .map(res => res.json());
   }
 
   storeToken(token) {
 
   }
 
-  setUser(token){
-
+  setUser(token) {
     this.username = this.jwtHelper.decodeToken(token).user;
     this.role = this.jwtHelper.decodeToken(token).role;
   }
